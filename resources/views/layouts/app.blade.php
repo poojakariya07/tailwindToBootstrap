94e52e2b5d65c77ff3ccbc24056ea79f94fc9c60
pooja <poojakariya1207@gmail.com>
2023-09-15 12:21:55 +0530

    changes in layout folder files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,14 @@
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         @vite(['resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body>
+        <div>
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-white shadow opacity-75">
+                    <div class=" py-3 px-4">
                         {{ $header }}
                     </div>
                 </header>
