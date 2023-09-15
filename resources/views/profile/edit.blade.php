45f7a9e857d43b1077f0765d8d996a05e4a42692
pooja <poojakariya1207@gmail.com>
2023-09-15 12:23:34 +0530

    changes in profile parties

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="fw-bold fs-5 text-secondary lh-1.2">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="mx-auto sm:px-6  py-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                <div>
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
