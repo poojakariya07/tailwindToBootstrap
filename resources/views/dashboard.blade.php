eb688d1af9f7fdc7583733a6bd3c14404fc619d2
pooja <poojakariya1207@gmail.com>
2023-09-15 12:24:10 +0530

    changes in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h4 class="font-weight-bold text-black py-4">
             {{ __('Dashboard') }}
-        </h2>
+        </h4>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+    <div class="p-5">
+        <div class=" mx-auto sm:px-6 ">
+            <div class="bg-white p-4 shadow p-3 mb-5 bg-body rounded">
+                <div class="m-6 text-secondary">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
